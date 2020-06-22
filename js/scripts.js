9f6032baaafb362e136998b15e42967388cab0a1
Brad <brad@learnwebcode.com>
2020-06-22 12:50:34 -0700

    Fixed main script file to match course files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 import "../css/style.css"
 
 // Our modules / classes
-import MobileMenu from './modules/MobileMenu';
-import HeroSlider from './modules/HeroSlider';
+import MobileMenu from "./modules/MobileMenu"
+import HeroSlider from "./modules/HeroSlider"
+
 
 // Instantiate a new object using our modules/classes
-var mobileMenu = new MobileMenu();
-var heroSlider = new HeroSlider();
+var mobileMenu = new MobileMenu()
+var heroSlider = new HeroSlider()
+
+
+// Allow new JS and CSS to load in browser without a traditional page refresh
+if (module.hot) {
+  module.hot.accept()
+}
