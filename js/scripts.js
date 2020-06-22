582cd49dda9ffff1232bcc4cb5508621063e890c
Brad <brad@learnwebcode.com>
2020-06-22 12:51:13 -0700

    Fix main js file.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,6 @@ import HeroSlider from "./modules/HeroSlider"
 var mobileMenu = new MobileMenu()
 var heroSlider = new HeroSlider()
 
-
 // Allow new JS and CSS to load in browser without a traditional page refresh
 if (module.hot) {
   module.hot.accept()
