f240986874703fe828097a4380898a69bc062ff5
Brad <brad@learnwebcode.com>
2021-07-09 11:18:09 -0700

    New folder structure for use with official wordpress/scripts solution

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import MobileMenu from "./modules/MobileMenu"
 import HeroSlider from "./modules/HeroSlider"
 
 // Instantiate a new object using our modules/classes
-var mobileMenu = new MobileMenu()
-var heroSlider = new HeroSlider()
+const mobileMenu = new MobileMenu()
+const heroSlider = new HeroSlider()
